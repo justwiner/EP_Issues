dd4d01f4c4bdbaae61026214da845db192325497
justwiner <392110917@qq.com>
2021-04-23 20:00:10 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="address" label="地址"> </el-table-column>
+    <el-table-column label="按钮">
+      <template #default="{ row }">
+        <el-button v-show="row.isShow"> 按钮 </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+export default {
+  setup() {
+    const tableData = ref([]);
+    onMounted(() => {
+      axios.get("https://assets.winerlu.com/temp/data.json").then((res) => {
+        tableData.value = res.data;
+      });
+    });
+    return {
+      tableData
+    };
+  },
+};
+</script>
